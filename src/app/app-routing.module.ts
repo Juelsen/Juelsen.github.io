0dfff7245efd7f07e8562ea9bbc70125be3618be
BernhardGundel <bernhard.gundel@yahoo.de>
2024-07-23 18:46:04 +0200

    WIP

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstScreenComponent } from './views/first-screen/first-screen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: FirstScreenComponent,
+    pathMatch: 'full',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
