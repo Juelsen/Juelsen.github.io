1d0e0851f6e4fff36c13aad8f26936f301b28a59
BernhardGundel <bernhard.gundel@yahoo.de>
2024-07-23 19:24:24 +0200

    WIP

--- src/app/views/second-screen/second-screen.component.ts
+++ src/app/views/second-screen/second-screen.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Howl } from 'howler';
 import { finalize, interval, map, take } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-second-screen',
@@ -11,8 +12,10 @@ export class SecondScreenComponent implements OnInit {
 
   private readonly COUNTDOWN_SECONDS = 20;
 
-  private timer$ = interval(1000).pipe(take(this.COUNTDOWN_SECONDS + 1));
-  countdown$ = this.timer$.pipe(map(n => this.COUNTDOWN_SECONDS - n), finalize(() => this.SOUND.pause()));
+  private timer$ = interval(150).pipe(take(this.COUNTDOWN_SECONDS + 1));
+  private countdown$ = this.timer$.pipe(map(n => this.COUNTDOWN_SECONDS - n), finalize(() => this.SOUND.pause()));
+
+  countdownSig = toSignal<number, number>(this.countdown$, { initialValue: this.COUNTDOWN_SECONDS });
 
   private readonly SOUND = new Howl({
     src: ['assets/audio/intro_audio.wav'],
